60cb0ab5ec188dca136590a435ce49b4e1ad4b91
samibashir795 <samibashir525@gmail.com>
2026-09-12 12:09:07 +0530

    Add files via upload

---
+++ positiveOdd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n > 0 && n % 2 != 0)
+        cout << "The number is positive and odd.";
+
+    return 0;
+}
